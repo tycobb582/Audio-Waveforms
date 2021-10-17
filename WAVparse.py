b33024de40b0cef147e64631e14214ee4c808f42
Elijah <aberjack776@gmail.com>
2021-10-17 00:10:30 -0400

    Added better file I/O

--- WAVparse.py
+++ WAVparse.py
@@ -4,6 +4,8 @@ import scipy.io.wavfile as wf
 import numpy as np
 import time
 import sys
+import random
+import math
 
 ##################################INITIALIZATION##############################
 
@@ -20,19 +22,40 @@ except:
 def get_wave_array(file_name_str):
     rate, data = wf.read(file_name_str)
     wave = data[:,0]
-    return wave
+    return wave, rate
                         
 def print_wave(wave):
     np.set_printoptions(threshold=sys.maxsize)
     print(wav)
 
-def create_wave(wave, speed=1, file_name="output.wav"):
-    rate = 44100 * speed
+def create_wave(wave, rate=44100, file_name="output.wav"):
     wf.write(file_name, rate, wave)
+
+def noise(wave, static_factor=10000):
+    for i in range(len(wave)):
+        wave[i] += random.randint(-static_factor,static_factor)
+    return wave
+
+def sin(wave, sin_factor=2000):
+    j = 0
+    for i in range(len(wave)):
+        tmp = math.sin(j) * sin_factor
+        #print(f"tmp{tmp} wave{wave[i]}")
+        wave[i] += tmp
+        j += 0.0006
+    return wave
+
+def test(wave):
+    for i in range(len(wave)):
+        step = wave[i] - wave[i-1]
+        wave[i] += step
+    return wave
 
 ##################################MAIN#######################################
 
-wav = get_wave_array(input)
-print_wave(wav)
+wav, rate = get_wave_array(input)
+#print_wave(wav)
+wave = test(wav)
+
 print(f"Program executed in {time.time()-start_time} seconds")
-create_wave(wav)
+create_wave(wav, rate=rate, file_name=input[:-4]+"out"+".wav")
