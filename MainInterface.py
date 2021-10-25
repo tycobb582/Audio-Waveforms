6d8ff9fff4d6ad37e67c09c49c8c09c98467b7a2
Elijah <aberjack776@gmail.com>
2021-10-25 14:03:23 -0400

    Added Visual Interace
    This isn't finished yet and won't really work. I just wanted to back up what I have so far

---
+++ MainInterface.py
@@ -0,0 +1,100 @@
+# the foundation for this code was taken from https://www.youtube.com/watch?v=4rzpMA6CUPg
+# and https://stackoverflow.com/questions/23154400/read-the-data-of-a-single-channel-from-a-stereo-wave-file-in-python
+import scipy.io.wavfile as wf
+import numpy as np
+import time
+import sys
+import random
+import math
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QInputDialog
+from PyQt5.QtCore import pyqtSlot
+import WAVparse as WAVp
+##################################INITIALIZATION##############################
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.win_width = 800
+        self.win_height = 600
+        self.rate = 0
+        self.final_wave_array = []
+        self.save_file_name = "output"
+        self.eigs = []
+        self.eig_vals = []
+        self.setGeometry(400, 400, self.win_width, self.win_height)
+        self.in_folder = "Audio files"
+        self.out_folder = "Outputs"
+        self.setWindowTitle("Eigenwave 9000")
+
+        self.in_folder_button = QPushButton("Input Folder", self)
+        self.in_folder_button.move(self.win_width // 4, self.win_height // 20 * 2)
+        self.in_folder_button.clicked.connect(self.in_pick)
+
+        self.build_mat_button = QPushButton("Build Matrix", self)
+        self.build_mat_button.move(self.win_width // 4 * 2, self.win_height // 20 * 2)
+        self.cov = []
+        self.build_mat_button.clicked.connect(self.build_matrix)
+
+        self.output_button = QPushButton("Create File", self)
+        self.output_button.move(self.win_width // 4 * 2, self.win_height // 20 * 4)
+        self.output_button.clicked.connect(self.do_output)
+
+        self.out_pick_button = QPushButton("Output Folder", self)
+        self.out_pick_button.move(self.win_width // 4, self.win_height // 20 * 4)
+        self.out_pick_button.clicked.connect(self.out_pick)
+
+        self.show()
+
+    @pyqtSlot()
+    def in_pick(self):
+        self.in_folder, done1 = QInputDialog.getText(
+            self, 'Input Dialog', 'Source Folder Name:')
+        print(f"changed input folder to \"{self.in_folder}\"")
+
+    @pyqtSlot()
+    def build_matrix(self):
+        start_time = time.time()
+        max_length = 10000000000
+        vecs = []  # blank list for wave vectors
+        # try:
+        for file in os.listdir(self.in_folder):
+            file_name = os.path.join(self.in_folder, file)
+            wav, self.rate = WAVp.get_wave_array(file_name)  # find vector from file
+            max_length = min(wav.size, max_length)
+            vecs.append(wav)  # add the vector to the list]
+        clamped_vecs = []
+        for vec in vecs:
+            clamped_vecs.append(clamp(vec, max_length))
+            print(len(vec))
+        print(self.final_wave_array)
+        mat = np.asarray(clamped_vecs)
+        # mat = np.matrix(*clamped_vecs)
+        self.cov = np.cov(mat)
+        self.eigs, self.eig_vals = np.linalg.eig(self.cov)
+        r_coefs = []
+        for i in range(len(self.eigs[0])):
+            r_coefs.append(random.randint(-100, 100))
+        # linear_combination = np.linalg.solve(self.eigs, self.eig_vals)
+        linear_combination = np.linalg.solve(self.eigs[0], self.eigs[1])
+        self.final_wave_array = linear_combination
+        print(f"Program executed in {time.time() - start_time} seconds")
+        # except:
+        #    print(f"{self.in_folder}is not a valid file path. Please change input folder name")
+
+    @pyqtSlot()
+    def out_pick(self):
+        self.out_folder, _ = QInputDialog.getText(self, 'Input Dialog', 'Output Folder Name:')
+        print(f"set output folder to \"{self.out_folder}\"")
+
+    @pyqtSlot()
+    def do_output(self):
+        self.save_file_name, _ = QInputDialog.getText(self, 'Input Dialog', 'Output File Name:')
+        # try:
+        wf.write(self.out_folder + "/" + self.save_file_name + ".wav", self.rate, self.final_wave_array)
+        print("created wave file")
+        # except:
+        #    print(self.out_folder+"/"+self.save_file_name+".wav" + " is not a valid output directory. Change it or else")
+
+app = QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec_())
